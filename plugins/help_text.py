32e0ae41faceb2a57ba645eebf28c1c72201dc91
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-11 17:17:06 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -73,7 +73,7 @@ async def upgrade(bot, update):
     TRChatBase(update.from_user.id, update.text, "/jihaad")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.JUHUDI,
+        text=Translation.JUHUDI_TXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True

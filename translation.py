7d665187aa89c66eec1da423060705191d9e5f5a
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-11 17:16:01 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -7,7 +7,7 @@ href='https://telegra.ph/Jihaad-01-24'>JUHUDI 👀</a>
 👉 /help Kwa Maelekezo Zaidi..
 Karibu 👉 @Hamisbot"""
     ILOVEISLAM_TEXT = "https://telegra.ph/i-love-islam-01-24."    
-    JUHUDI = "https://telegra.ph/Jihaad-01-24."
+    JUHUDI_TXT = "https://telegra.ph/Jihaad-01-24."
     HELP_USER = """Asslaam Aleykum Warahmatullaah Wabarakaatuh
 Ndugu Katika Imaan Mimi ni Bot ninaye husika na kukuelimisha Mambo kadhaa Ya Jihaad Kwa Ajili ya Allaah
 Kwa sasa Bado Niko Katima *MATENGENEZO* In Shaa Allah Edelea kusoma Elimu iliyopo, Karibu Katika Kheri"""

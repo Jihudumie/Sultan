5bea4710457e9a6d06aef2a1d2198583b45c18e8
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-11 17:23:53 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,14 +1,14 @@
 class Translation(object):
-    START_TEXT = """Karibu,
+    START_TEXT = "Karibu,
 Karibu Ujifunze Harakati Za Kujikomboa kwa Kwa Waislam Kujitoa katika Utawala wa Kikafiri
 Soma 👇
 <a
 href='https://telegra.ph/Jihaad-01-24'>JUHUDI 👀</a>
 👉 /help Kwa Maelekezo Zaidi..
-Karibu 👉 @Hamisbot"""
+Karibu 👉 @Hamisbot"
     ILOVEISLAM_TEXT = "https://telegra.ph/i-love-islam-01-24."    
     JUHUDI_TXT = "https://telegra.ph/Jihaad-01-24."
-    HELP_USER = """Asslaam Aleykum Warahmatullaah Wabarakaatuh
+    HELP_USER = "Asslaam Aleykum Warahmatullaah Wabarakaatuh
 Ndugu Katika Imaan Mimi ni Bot ninaye husika na kukuelimisha Mambo kadhaa Ya Jihaad Kwa Ajili ya Allaah
-Kwa sasa Bado Niko Katima *MATENGENEZO* In Shaa Allah Edelea kusoma Elimu iliyopo, Karibu Katika Kheri"""
+Kwa sasa Bado Niko Katima *MATENGENEZO* In Shaa Allah Edelea kusoma Elimu iliyopo, Karibu Katika Kheri"
    HAMIS_MAJIBU = "Khamis Ni mtengenezaji wa Bot Aina Mbali Mabli Wasiliana nae kwa Kazi."

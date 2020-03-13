a71731ac7413230d2b1b6c74e88dfa811793a736
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-13 13:03:11 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -69,14 +69,3 @@ async def start(bot, update):
         reply_to_message_id=update.message_id
     )
 
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["hamis", "jihaad"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/hamis")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.HAMIS_MAJIBU,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-    )

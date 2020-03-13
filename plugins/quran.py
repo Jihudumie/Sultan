8624052f1c78fe7d02d588c474efdea2ffbdae43
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-13 13:04:04 +0300

    Update quran.py

--- plugins/quran.py
+++ plugins/quran.py
@@ -36,3 +36,14 @@ async def upgrade(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id,
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["hamis", "jihaad"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/hamis")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HAMIS_MAJIBU,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+    )

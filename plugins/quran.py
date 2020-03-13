1457322ad9ddf6681dba5685f74b060b9dddf66f
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-13 12:57:00 +0300

    Create quran.py

---
+++ plugins/quran.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["quraan"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/quraan")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.QURAAN_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+    )

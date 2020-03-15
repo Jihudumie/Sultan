dcf759b45ae26baf375918b2c8b735791399ac8c
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-15 23:08:05 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,1 +1,30 @@
 
+from io import BytesIO
+from time import sleep
+from typing import Optional, List
+from telegram import TelegramError, Chat, Message
+from telegram import Update, Bot
+from telegram.error import BadRequest
+from telegram.ext import MessageHandler, Filters, CommandHandler
+from telegram.ext.dispatcher import run_async
+from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
+
+import tg_bot.modules.sql.users_sql as sql
+from tg_bot import dispatcher, OWNER_ID, LOGGER
+from tg_bot.modules.helper_funcs.filters import CustomFilters
+
+
+@run_async
+def snipe(bot: Bot, update: Update, args: List[str]):
+    try:
+        chat_id = str(args[0])
+        del args[0]
+    except TypeError as excp:
+        update.effective_message.reply_text("Please give me a chat to echo to!")
+    to_send = " ".join(args)
+    if len(to_send) >= 2:
+        try:
+            bot.sendMessage(int(chat_id), str(to_send))
+        except TelegramError:
+            LOGGER.warning("Couldn't send to group %s", str(chat_id))
+            update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")

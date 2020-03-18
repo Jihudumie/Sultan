60837a54a59f2572a2aabaad10b4dd099c9c4164
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-18 08:19:55 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,25 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
 
-from io import BytesIO
-from time import sleep
-from typing import Optional, List
-from telegram import TelegramError, Chat, Message
-from telegram import Update, Bot
-from telegram.error import BadRequest
-from telegram.ext import MessageHandler, Filters, CommandHandler
-from telegram.ext.dispatcher import run_async
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Free User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(592800547)
+    return expires_at
 
 
-@run_async
-def snipe(bot: Bot, update: Update, args: List[str]):
-    try:
-        chat_id = str(args[0])
-        del args[0]
-    except TypeError as excp:
-        update.effective_message.reply_text("Please give me a chat to echo to!")
-    to_send = " ".join(args)
-    if len(to_send) >= 2:
-        try:
-            bot.sendMessage(int(chat_id), str(to_send))
-        except TelegramError:
-            LOGGER.warning("Couldn't send to group %s", str(chat_id))
-            update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/help")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
+async def get_me_info(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/me")
+    chat_id = str(update.from_user.id)
+    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT,
+        reply_to_message_id=update.message_id
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["hamis", "jihaad"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/hamis")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HAMIS_MAJIBU,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+
+    )

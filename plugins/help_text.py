8908cb2afe7a333d59fbf79dd520eaa94e0f665c
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-31 14:34:53 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -70,14 +70,16 @@ async def start(bot, update):
     )
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["quran"]))
-async def quran(bot, update):
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
     # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/quran")
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.QURAN_TEXT,
-        reply_to_message_id=update.message_id
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
     )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["hamis", "jihaad"]))
@@ -88,5 +90,5 @@ async def upgrade(bot, update):
         chat_id=update.chat.id,
         text=Translation.HAMIS_MAJIBU,
         parse_mode="html",
-        reply_to_message_id=update.message_id
+        reply_to_message_id=update.message_id,
     )

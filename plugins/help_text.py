6f8352f92a7ff16ad07fb73efa816e186dd5ca93
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-03-31 14:53:00 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -92,3 +92,14 @@ async def upgrade(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id,
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["kitabu"]))
+async def kitabu_kitukufu(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/kitabu")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.KITABU_KITUKUFU,
+        reply_to_message_id=update.message_id
+    )
+

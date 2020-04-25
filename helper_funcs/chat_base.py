b9ec748d488bc962b1836c6091ff31c9dc800c30
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-25 15:43:31 +0300

    Update chat_base.py

--- helper_funcs/chat_base.py
+++ helper_funcs/chat_base.py
@@ -17,19 +17,4 @@ else:
     from config import Config
 
 # the Strings used for this "thing"
-from translation import Translation
-
-from pyrogram import Client, Filters
-
-# the Telegram trackings
-from chatbase import Message
-
-
-def TRChatBase(chat_id, message_text, intent):
-    msg = Message(api_key=Config.CHAT_BASE_TOKEN,
-              platform="Telegram",
-              version="1.3",
-              user_id=chat_id,
-              message=message_text,
-              intent=intent)
-    resp = msg.send()
+from translation 

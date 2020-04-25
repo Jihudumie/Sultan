0aa59fff769be3faff447a91763eddec045ac84c
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-04-25 07:32:57 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# (c) Khamis Seleleko
 
 # the logging things
 import logging
@@ -25,6 +25,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 
+
 @pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
 async def help_user(bot, update):
     # logger.info(update)
@@ -32,21 +33,6 @@ async def help_user(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_USER,
-        parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
-async def get_me_info(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/me")
-    chat_id = str(update.from_user.id)
-    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
@@ -63,19 +49,6 @@ async def start(bot, update):
         reply_to_message_id=update.message_id
     )
 
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
-
 @pyrogram.Client.on_message(pyrogram.Filters.command(["hamis", "jihaad"]))
 async def upgrade(bot, update):
     # logger.info(update)
@@ -85,25 +58,4 @@ async def upgrade(bot, update):
         text=Translation.HAMIS_MAJIBU,
         parse_mode="html",
         reply_to_message_id=update.message_id,
-    )
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["kitabu"]))
-async def kitabu_kitukufu(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/kitabu")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.KITABU_KITUKUFU,
-        reply_to_message_id=update.message_id
-    )
-
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["quran"]))
-async def quran_tukufu(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/quran")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.QURAN_TUKUFU,
-        reply_to_message_id=update.message_id
     )

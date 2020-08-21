9e4cd26c0c3ac9e0eaf1caa2703c08fbff15b60f
Jihudumie <61816001+Jihudumie@users.noreply.github.com>
2020-08-21 16:19:22 +0300

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -58,3 +58,14 @@ async def upgrade(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id,
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["wakweli", "almuhajir"]))
+async def wakweli(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "wakweli")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.WAKWELI,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+    )
